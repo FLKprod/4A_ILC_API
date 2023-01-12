587f2c5545c8b58bf202d522cedea26fbfb3fe00
Luke <65294756+LukeDvy@users.noreply.github.com>
2023-01-12 11:29:16 +0100

    Première modification du code pour l'adapter au projet
    Création du dictionnaire de transaction, de la classe Transaction, et de la méthode recevant une requête GET

--- myFlask.py
+++ myFlask.py
@@ -2,9 +2,35 @@ from flask import Flask
 from flask import request
 
 app = Flask(__name__)
-dict={ 0:"henru",1: "romain", 2:"quentin"}
 
-@app.route("/", methods=['GET','PUT']) #appel dans un aurtre terminal avec : curl -X GET http://127.0.0.1:5000
+class Transaction:
+    def __init__(self,P1:str,P2:str,t:str,s:float):
+        self.P1=P1
+        self.P2=P2
+        self.t=t
+        self.s=s
+
+t1=Transaction(5,5,5,5)
+t2=Transaction(5,5,5,5)
+dict={1:t1,2:t2}
+
+@app.route("/", methods=['GET']) #appel dans un aurtre terminal avec : curl -X GET http://127.0.0.1:5000
+def login():
+    if request.method == 'GET':
+        vueDict=""
+        for i in range(len(dict)+1):
+            if(i>0):
+                vueDict+="Transaction de : "+str(dict[1].P1)+" vers : "+str(dict[1].P2)+" a l'heure "+str(dict[1].t)+" de la somme :"+str(dict[1].s)+"€"
+        return vueDict
+'''
+@app.route("/<P1>/<P2>/<t>/<s>/", methods=['PUT']) #appel dans un aurtre terminal avec : curl -X GET http://127.0.0.1:5000
+def ajoutTransaction(P1=None,P2=None,t=None,s=None): #E2 : méthode permettant d'enregistrer une transaction
+    if request.method == 'PUT':
+        transaction=Transaction(P1,P2,t,s)
+        dict[len(dict)]=transaction
+        return dict'''
+
+'''@app.route("/", methods=['GET','PUT']) #appel dans un aurtre terminal avec : curl -X GET http://127.0.0.1:5000
 def login():
     if request.method == 'GET':
         vueDict=str(dict)
@@ -23,7 +49,7 @@ def loge(key=None):
 def loinPost(key=None, nom=None):
     if request.method == 'POST':
         dict[int(key)]=nom
-        return dict
+        return dict'''
 
 
 if __name__ == '__main__':
