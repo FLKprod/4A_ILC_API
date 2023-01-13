2b79afa1f98a55fb9921cafc8c46b1d6b7205971
Luke <65294756+LukeDvy@users.noreply.github.com>
2023-01-13 09:00:53 +0100

    Ajout test pour build app

--- myFlask.py
+++ myFlask.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 import json
+import sys
 
 app = Flask(__name__)
 
@@ -83,4 +84,11 @@ def loinPost(key=None, nom=None):
 
 
 if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "check_syntax":
+            print("Build [OK]")
+            exit(0)
+        else:
+            print("Passed argument not supported ! Supported argument : check_syntax")
+            exit(1)
     app.run()
