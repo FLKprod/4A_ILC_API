b96efc53bd7a13315e02ac418948ebdbabe6e6e3
Luke <65294756+LukeDvy@users.noreply.github.com>
2023-01-23 17:08:12 +0100

    Ajout accès CSV et fix : assemblement des deux fichiers (détail en description)
    Assemblement du fichier myFlask.py (fichier principal du projet des transactions), avec getValuesFromCSV.py permettant l'accès aux données du fichier data.csv
    Fix : ajout de saut de ligne dans les requêtes GET, pour l'affichage sur page HTML

--- myFlask.py
+++ myFlask.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 import json
 import sys
+import csv
 
 app = Flask(__name__)
 
@@ -23,11 +24,42 @@ class Transaction:
         self.t=t
         self.s=s
 
+#Création des comptes
 p1=Personne("jean",5000)
 p2=Personne("louis",4000)
+p3=Personne("Paul",4000)
+p4=Personne("Luke",4000)
+p5=Personne("Jean",4000)
+p6=Personne("Nicolas",4000)
+p7=Personne("Mathis",4000)
+p8=Personne("Nasser",4000)
 
-personne={0:p1,1:p2}
+#Instanciation des dictionnaires : personne (composé d'objets 'Personne'), dict (composé par la suite d'objet 'Transaction')
+personne={0:p1,1:p2,2:p3,3:p4,4:p5,5:p6,6:p7,7:p8}
 dict={}
+tab = []
+
+#Ouverture du fichier CSV
+with open('data.csv', newline='', encoding="utf-8-sig") as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        for rows in row:
+            p = (str(rows).split(';'))
+            tab.append(p)
+
+#Boucle permettant de parcourir le fichier CSV, et de créer les transactions définies, en cherchant pour chaque transactions les personnes associées
+for i in range(1, 7):
+    for j in range(len(personne)):
+            if(personne[j].nom == str(tab[i][0])):
+                _P1=personne[j]
+                personne[j].debit(float(tab[i][3]))
+            if(personne[j].nom == str(tab[i][1])):
+                _P2=personne[j]
+                personne[j].reception(float(tab[i][3]))
+    tempsT = tab[i][2]
+    sommeS = tab[i][3]
+    transaction=Transaction(_P1,_P2,str(tempsT),float(sommeS))
+    dict[len(dict)+1]=transaction
 
 @app.route("/", methods=['GET']) #appel dans un aurtre terminal avec : curl -X GET http://127.0.0.1:5000
 def affichage(): #afficher toutes les transactions
@@ -35,7 +67,7 @@ def affichage(): #afficher toutes les transactions
         vueDict=""
         for i in range(len(dict)+1):
             if(i>0):
-                vueDict+="Transaction de : "+str(dict[i].P1.nom)+" vers : "+str(dict[i].P2.nom)+" a l'heure "+str(dict[i].t)+" de la somme :"+str(dict[i].s)+"€"
+                vueDict+="Transaction de : "+str(dict[i].P1.nom)+" vers : "+str(dict[i].P2.nom)+" a l'heure "+str(dict[i].t)+" de la somme :"+str(dict[i].s)+"€"+"<br>"
         return vueDict
 
 @app.route("/nom/<_p>", methods=['GET']) #appel dans un aurtre terminal avec : curl -X GET http://127.0.0.1:5000
@@ -44,7 +76,7 @@ def affichageNom(_p=None): #afficher toutes les transactions liées à une perso
         vueDict=""
         for i in range(len(dict)+1):
             if(i>0 and ((dict[i].P1.nom == str(_p)) or (dict[i].P2.nom == str(_p)))):
-                vueDict+="Transaction de : "+str(dict[i].P1.nom)+" vers : "+str(dict[i].P2.nom)+" a l'heure "+str(dict[i].t)+" de la somme :"+str(dict[i].s)+"€"
+                vueDict+="Transaction de : "+str(dict[i].P1.nom)+" vers : "+str(dict[i].P2.nom)+" a l'heure "+str(dict[i].t)+" de la somme :"+str(dict[i].s)+"€"+"<br>"
         return vueDict
 
 @app.route("/solde/<_p>", methods=['GET']) #appel dans un aurtre terminal avec : curl -X GET http://127.0.0.1:5000
